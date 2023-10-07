133a93fb14ad2fc808a9887e083f45d0f03e8dc9
Lucy Fidock <lucyfidock@Lucys-MacBook-Air-4.local>
2023-10-07 16:12:42 +1030

    try

--- Timer.cpp
+++ Timer.cpp
@@ -1,3 +1,15 @@
 #include "Timer.h"
 
-void Timer::startTimer() {}
+void Timer::startTimer() {
+    Time = time(0);
+}
+
+void Timer::resetTimer() {
+    Time = time(0);
+}
+
+void Timer::stopTimer() {}
+
+void Timer::onLeftClick() {}
+
+void Timer::onRightClick() {}
--- Timer.h
+++ Timer.h
@@ -2,12 +2,20 @@
 #define TIMER_H
 
 #include "Entity.h"
+#include <ctime>>
 
 using namespace sf;
 
 class Timer: Entity {
+    private:
+    std::time_t Time;
+    //display text;
     public:
         void startTimer();
+        void stopTimer();
+        void resetTimer();
+        void onLeftClick();
+        void onRightClick();
 };
 
 #endif
