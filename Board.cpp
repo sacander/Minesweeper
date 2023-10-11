ca3982bbfe34419f199454e66be159f18c481991
yakk1 <yakkazem@gmail.com>
2023-10-12 00:44:05 +1030

    fixed bug on reveal tiles

--- Board.cpp
+++ Board.cpp
@@ -16,18 +16,19 @@ bool Board::incrementRevealedTiles() {
 //Reveals all mines when game is lost
 void Board::revealMines(){
 
-    Mine* temp = new Mine;
-
-    for (int i = 0; i < xSize; i++)
-    {
-        for (int j = 0; j < ySize; j++)
-        {
-            //if type of tile is a mine reveal it
-            if (typeid(*temp) == typeid(*tiles[i][j]))
-            {
+//Reveal mines
+    for (int i = 0; i < xSize; i++){
+        for (int j = 0; j < ySize; j++){
+            //Check if tile is a mine or not (based on a known mine location)
+            if (swappedTiles){
+                if (typeid(*tiles[firstSafeX][firstSafeY]) == typeid(*tiles[i][j])){
                 tiles[i][j]->showTile();
+                }
+            } else {
+                if (typeid(*tiles[firstSafeX][firstSafeY]) != typeid(*tiles[i][j])){
+                tiles[i][j]->showTile();
+                }
             }
-            
         }
     }
 }
@@ -36,6 +37,7 @@ void Board::swapTiles(int x, int y) {
     Tile* temp = tiles[x][y];
     tiles[x][y] = tiles[firstSafeX][firstSafeY];
     tiles[firstSafeX][firstSafeY] = temp;
+    swappedTiles = true;
 }
 
 //Returns an array of addresses to tiles that are adjacent
@@ -48,25 +50,25 @@ std::vector<Tile **> Board::getAdjacentTiles(int x, int y) {
     //Add adj tiles to the left (if not on a border)
     if (x > 0){
        for (int i = 0; i < 3; i++){
-            adjTiles.push_back(tiles[x-1][y - 1 + i]);
+            adjTiles.push_back(&tiles[x-1][y - 1 + i]);
        }
     }
 
     if (x < xSize - 1){
        for (int i = 0; i < 3; i++){
-            adjTiles.push_back(tiles[x+1][y - 1 + i]);
+            adjTiles.push_back(&tiles[x+1][y - 1 + i]);
        }
     }
     
     if (y > 0){
-        adjTiles.push_back(tiles[x][y - 1]);
+        adjTiles.push_back(&tiles[x][y - 1]);
     }
 
     if (y < ySize - 1){
-        adjTiles.push_back(tiles[x][y + 1]);
+        adjTiles.push_back(&tiles[x][y + 1]);
     }
 
-    return adjTiles
+    return adjTiles;
 }
 
 int Board::getRevealedTiles() {
