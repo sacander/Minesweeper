9f761426d9629b57a0f53b7635a987f673d95c7b
Lucy Fidock <lucyfidock@student-10-201-42-063.wireless.adelaide.edu.au>
2023-10-12 13:48:45 +1030

    simple write and read from file implemented

--- LoadButton.cpp
+++ LoadButton.cpp
@@ -1,5 +1,7 @@
 #include "LoadButton.h"
 #include "Minesweeper.h"
+#include <fstream>
+#include <iostream>
 
 LoadButton::LoadButton(Vector2f pos, Minesweeper &game)
 : Entity("./textures/load.png", pos, game) {}
@@ -15,5 +17,12 @@ void LoadButton::onRightClick() {
 }
 
 void LoadButton::loadGame() {
-    //saving
+    //load from a file 
+    std::string dataLine;
+    std::ifstream saveFile("progress.txt");
+    if (saveFile.is_open()){
+        while (std::getline(saveFile, dataLine))
+            std::cout << dataLine << std::endl;
+    }
+    saveFile.close();
 }
