a4b56341024184aed8c9ca3ba9452f0b326bff1c
Lucy Fidock <lucyfidock@Lucys-MacBook-Air-4.local>
2023-10-14 21:12:04 +1030

    test for timer done

--- LoadButton.cpp
+++ LoadButton.cpp
@@ -19,10 +19,50 @@ void LoadButton::onRightClick() {
 void LoadButton::loadGame() {
     //load from a file 
     std::string dataLine;
-    std::ifstream saveFile("progress.txt");
+    std::ifstream saveFile("progress.csv");
+
+    int elapsedTime = 0;
+    int length = 0;
+    int width = 0;
+
     if (saveFile.is_open()){
-        while (std::getline(saveFile, dataLine))
-            std::cout << dataLine << std::endl;
+        //get timer info
+        std::getline(saveFile, dataLine, '\n');
+        elapsedTime = stoi(dataLine);
+        //std::cout << elapsedTime << std::endl;
+
+        //get length and width
+        std::getline(saveFile, dataLine, ',');
+        length = stoi(dataLine);
+        //std::cout << length << std::endl;
+
+        std::getline(saveFile, dataLine, '\n');
+        width = stoi(dataLine);
+
+        //create the array using length and width
+        int **boardArray = new int*[length];
+        for (int i = 0; i < length; i++) {
+            boardArray[i] = new int[width]{};
+        }
+
+        //read info into array
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                std::getline(saveFile, dataLine, ',');
+                boardArray[i][j] = stoi(dataLine);
+            }
+        }
+
+        for (int i = 0; i < length; i++){
+            for (int j = 0; j < width; j++) {
+                std::cout << boardArray[i][j];
+            }
+        std::cout << std::endl;
+    }
+
+
+     //while (std::getline(saveFile, dataLine))
+       //std::cout << dataLine << std::endl;
     }
     saveFile.close();
 }
